1b9f91089bfd4a99e634e1721a5424237efaf536
rakshhx <145245132+rakshhx@users.noreply.github.com>
2025-03-26 14:28:26 +0530

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My first Web page</h2>
+
+<p id="demo">JavaScript </p>
+
+<script>
+document.getElementById("demo").innerHTML = "Hello JavaScript!"
+</script>
+<!--Used for action and validation -->
+</body>
+</html>
